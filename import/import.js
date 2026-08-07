4ba26a0247a04fe4e8300b8d1d96ac34629e8061
mikimus986 <mikimus9@email.cz>
2026-08-07 13:42:00 +0200

    Update import.js

--- import/import.js
+++ import/import.js
@@ -1,1 +1,35 @@
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.5.136/pdf.worker.min.js';
 
+document.getElementById("convertButton").addEventListener("click", async () => {
+
+    const file = document.getElementById("pdfFile").files[0];
+
+    if (!file) {
+        alert("Vyber PDF.");
+        return;
+    }
+
+    const arrayBuffer = await file.arrayBuffer();
+
+    const pdf = await pdfjsLib.getDocument({
+        data: arrayBuffer
+    }).promise;
+
+    let text = "";
+
+    for (let page = 1; page <= pdf.numPages; page++) {
+
+        const p = await pdf.getPage(page);
+
+        const content = await p.getTextContent();
+
+        content.items.forEach(item => {
+            text += item.str + "\n";
+        });
+
+    }
+
+    document.getElementById("output").textContent = text;
+
+});
